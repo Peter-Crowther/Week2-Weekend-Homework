5f3c02a821958291073f6a5a0b94b5df8c97600f
Peter Crowther <petercrowther90@hotmail.com>
2017-10-06 13:33:37 +0100

    created more guests

--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -6,11 +6,18 @@ class TestGuest < MiniTest::Test
 
   def setup
     @guest = Guest.new("Mark")
+    @guest2 = Guest.new("Tony")
+    @guest3 = Guest.new("Suzi")
   end
 
   def test_guest_has_name()
     actual = @guest.name()
     assert_equal("Mark", actual)
+  end
+
+  def test_guest_has_name()
+    actual = @guest2.name()
+    assert_equal("Tony", actual)
   end
 
 
